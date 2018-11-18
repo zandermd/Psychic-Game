8ff4906bcc1702a79593c4248e32ff633b983f0d
Zander Dyer <zanderdyer@Zanders-MacBook-Pro.local>
2018-11-17 17:09:09 -0700

    added js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,16 @@
 //make counters & varibles
 var won = 0;
 var lost = 0;
-var attempts = 10;
+var guesses = 10;
+var choices = [];
 var emptyString = "";
-var ranLetter = ranLetter;
-var letters = "abcdefghijklmnopqrstuvwxyz";
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
-//generate a random letters
-function getRandomLetter() {
-ranLetter = letters[Math.floor(Math.random() * letters.length)];
-console.log(ranLetter);
-}
+//generate random letters
+var ranLetter = letters[Math.floor(Math.random() * letters.length)];
+console.log(ranLetter)
+
+//log users input, make click event
+
+
+
